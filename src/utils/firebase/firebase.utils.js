3a3552c6f6627576bb0a67b613f43bedbc5c13b9
John Martinez <johnmartinez0408@gmail.com>
2023-04-05 13:16:42 -0400

    added code to automatically create users in firebase database on google login

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // web app's Firebase configuration
 const firebaseConfig = {
@@ -26,3 +27,29 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const database = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) => {
+  const userDocRef = doc(database, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  //if user doesn't exist, create user in database
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
